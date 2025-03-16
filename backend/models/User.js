5e2e2c9f24ad1cdc4ffe1997545ea3d647192fe7
abdullah <abdullahilyassaeed@gmail.com>
2025-03-16 20:25:51 +0500

    env added

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-mongoose.connect('mongodb://127.0.0.1:27017/kanban', {
+mongoose.connect('process.env.MONGODB_URI', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(
