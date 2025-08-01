cc8803314e074e8d7ec5339ebd56420b1b34d299
abdullah <abdullahilyassaeed@gmail.com>
2025-08-01 17:34:52 +0500

    Add frontend deployment endpoint

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://abdullahkanbans.netlify.app/",
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: true,
         allowedHeaders: ['Authorization', 'Content-Type'],
