c63af89e7ff39530da6142d78c9768e1cdb2a730
abdullah <abdullahilyassaeed@gmail.com>
2025-08-01 18:14:38 +0500

    setup frontendt domain resolving

--- backend/app.js
+++ backend/app.js
@@ -24,9 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["http://localhost:5173","https://abdullahkanbans.netlify.app",
-            
-        ],
+        origin: ["http://localhost:5173","https://kanban-boards-task-management.netlify.app"],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: true,
         allowedHeaders: ['Authorization', 'Content-Type'],
