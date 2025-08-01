1e5c9616dbfa16c5745ccddcded8f3a9fc5f2157
abdullah <abdullahilyassaeed@gmail.com>
2025-08-01 16:10:21 +0500

    serverless config

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const serverless = require('serverless-http');  // <--- add this
 const Board = require('./models/Board.js');
 const { createBoard, validateUser, authenticationuser } = require('./controllers/boards.js');
 const { signupMiddleware, validateSignup } = require('./controllers/signup.js');
 const { loginMiddleware, validatelogin } = require('./controllers/login.js');
 const { logoutMiddleware } = require('./controllers/logout.js');
-const {createTask , validateTask} = require('./controllers/createTask.js')
+const { createTask , validateTask } = require('./controllers/createTask.js');
 const helmet = require('helmet');
 const cors = require("cors");
 const { homeRoute } = require('./controllers/homeRoute.js');
@@ -14,11 +15,8 @@ const { checkUser } = require('./middleware/AuthenticateUser.js');
 const { updateBoard } = require('./controllers/updateController.js');
 const { deleteBoard } = require('./controllers/deleteController.js');
 const { updateTaskStatusInBoard } = require('./controllers/statusUpdateController.js');
-const {updateStatus,updateSubtask} = require("./controllers/UpdateTaskStatus.js")
+const { updateStatus, updateSubtask } = require("./controllers/UpdateTaskStatus.js");
 const { updateTask, deleteTask } = require('./controllers/TaskController.js');
-
-
-
 
 const app = express();
 
@@ -28,59 +26,36 @@ app.use(cookieParser());
 app.use(
     cors({
         origin: "http://localhost:5173",
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // <-- Add PATCH here
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: true,
         allowedHeaders: ['Authorization', 'Content-Type'],
     })
 );
 
 app.use(helmet());
-// -----------------------home route ---------------------
+
+// Routes
 app.get('/', homeRoute);
 
-
-//  signup route
 app.get('/signup', (req, res) => {
     res.send('hello world');
 });
 
-// create task
-
-app.post('/createTask/:id',validateTask, createTask);
-
+app.post('/createTask/:id', validateTask, createTask);
 app.put('/statusUpdate/:id', updateTaskStatusInBoard);
-
 app.patch("/tasks/:taskId/subtasks/:subtaskId", updateSubtask);
-
 app.patch("/tasks/:taskId/status", updateStatus);
-
 app.put("/tasks/:taskId", updateTask);
 app.delete("/tasks/:taskId", deleteTask);
 
-
-
-
-
-// create task
-
-
-
-// login  or logout  and signup route      
 app.post('/signup', validateSignup, signupMiddleware);
-
 app.post('/login', validatelogin, loginMiddleware);
 
-// boards routes
-
-
-app.post('/createBoard', authenticationuser , createBoard);
-
-app.put('/updateBoard/:id', updateBoard );
-
-app.delete('/deleteBoard/:id', deleteBoard );
-
+app.post('/createBoard', authenticationuser, createBoard);
+app.put('/updateBoard/:id', updateBoard);
+app.delete('/deleteBoard/:id', deleteBoard);
 app.get('/logout', logoutMiddleware);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+// Remove app.listen for Vercel serverless deployment
+// Export the serverless handler instead:
+module.exports.handler = serverless(app);
